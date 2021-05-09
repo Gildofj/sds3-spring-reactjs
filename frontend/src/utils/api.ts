a08a8b6315b17be1236180c84dda07b77b17e65a
Gildo Junior <1gildojunior@gmail.com>
2021-05-09 02:55:55 -0300

    Alter env config

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+const baseURL = process.env.BACKEND_URL ?? "http://localhost:8080";
 
 export const api = axios.create({
   baseURL,
